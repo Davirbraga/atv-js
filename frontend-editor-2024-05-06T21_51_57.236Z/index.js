b14da405ed10d39223ea2640236b99d866e8ee23
Davi Rodrigues Braga <davirbraga.r@gmail.com>
2024-05-06 18:54:36 -0300

    Primeira Aula
    ultimo código aplicado na aula de javaScript

---
+++ frontend-editor-2024-05-06T21_51_57.236Z/index.js
@@ -0,0 +1,14 @@
+let bernardo = [
+  'girafa',
+  'zebra',
+  24,
+  'alfabeto',
+  {
+    name: "Davi",
+    age: 26,
+    isAdm: true
+  },
+  true
+ ]
+ console.log(`A segunda do seu array é ${bernardo[4].name}`)
+
